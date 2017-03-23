cfc9ea71a5bb4dbbcddf9d8e166100350d170f3f
mvillegascuellar <mvillegascuellar@gmail.com>
2017-03-23 08:41:40 -0500

    Nuevas funciones para application settins y login permissions

---
+++ functions/Copy-tssPLSApplicationSettings.ps1
@@ -0,0 +1,67 @@
+﻿function Copy-tssPLSApplicationSettings{
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$Environment,
+        [parameter(Mandatory=$true)]
+        [string]$SourceSubEnvironment,
+        [parameter(Mandatory=$true)]
+        [string]$DestSubEnvironment
+    )
+
+    Write-Verbose "Preparando conexión a Origen"
+    $EnvServer = Get-tssConnection -Environment $Environment
+    [string]$SourcePLSDB = Get-tssDatabaseName -SQLServer $EnvServer -Environment $Environment -SubEnvironment $SourceSubEnvironment -Database PLS
+    if ($SourcePLSDB -eq $null -or $SourcePLSDB.Trim() -eq '') {
+        Write-Error "No es posible conectar a la base de datos PWB del Origen";
+        return $null
+    }
+
+    Write-Verbose "Preparando conexión a Destino"
+    [string]$DestPLSDB = Get-tssDatabaseName -SQLServer $EnvServer -Environment $Environment -SubEnvironment $DestSubEnvironment -Database PLS
+    if ($DestPLSDB -eq $null -or $DestPLSDB.Trim() -eq '') {
+        Write-Error "No es posible conectar a la base de datos PLS del Destino"
+        return $null
+    }
+
+    [string] $sqlCopyAppSettings = "declare @t_configurations TABLE
+                                    (GroupName varchar(50)
+                                    ,Name	varchar(50))
+
+                                    INSERT INTO @t_configurations 
+                                    VALUES ('Customer Order', 'AutoApproveAccessorialsReceiversList'),
+                                    ('CustomerInvoice', 'DefaultPrinter'),
+                                    ('CustomerInvoice', 'InvoicePrintFolder'),
+                                    ('CustomerInvoice', 'DocReqPrintFolder'),
+                                    ('Equipment', 'UserEDICreateEquipment'),
+                                    ('File Server', 'FileServerPath'),
+                                    ('GeocodingAPI', 'Key'),
+                                    ('MisuseReport', 'DistributionList'),
+                                    ('Orders', 'UserEDIUpdateOrders'),
+                                    ('Pricing', 'WarningMessageSendEmailUser'),
+                                    ('Pricing', 'DOEFuelIndexEmailSender'),
+                                    ('Quote', 'QuoteLaneRouteEmailAddress'),
+                                    ('Work Order', 'RailBillingEmail'),
+                                    ('Work Order', 'RSCNotificationCC'),
+                                    ('XpoRealTime', 'CustomerOrderBoardUrl'),
+                                    ('XpoRealTime', 'WorkOrderBoardUrl'),
+                                    ('XpoRealTime', 'OrderChargeBoardUrl'),
+                                    ('XpoRealTime', 'DashboardApiUrl');
+
+                                    update a
+                                    set a.Value = b.Value
+                                    ,UpdatedBy = 'RefreshEnv'
+                                    ,UpdateDate = getdate()
+                                    from ApplicationSetting a
+                                    inner join $SourcePLSDB.dbo.ApplicationSetting b
+                                    on a.GroupName = b.GroupName
+                                    and a.Name = b.Name
+                                    where EXISTS (SELECT 1
+	                                     FROM @t_configurations c
+	                                    WHERE a.GroupName = c.GroupName
+	                                    AND a.Name = c.Name)"
+
+    Write-Verbose "Copiando Application Settings"
+    Invoke-Sqlcmd2 -ServerInstance $EnvServer.Name -Database $DestPLSDB -Query $sqlCopyAppSettings
+
+}
---
+++ functions/Set-tssLoginPermissions.ps1
@@ -0,0 +1,94 @@
+﻿function Set-tssLoginPermissions {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$Environment,
+        [parameter(Mandatory=$true)]
+        [string]$SubEnvironment,
+        [switch]$SkipPWB
+    )
+
+    Write-Verbose "Preparando conexión a la base de datos"
+    $EnvServer = Get-tssConnection -Environment $Environment
+    [string]$PLSDBName = Get-tssDatabaseName -SQLServer $EnvServer -Environment $Environment -SubEnvironment $SubEnvironment -Database PLS
+    $PLSDBName
+    if ($PLSDBName -eq $null -or $PLSDBName.Trim() -eq '') {
+        Write-Error "No es posible conectar a la base de datos PLS";
+        return $null
+    }
+    $PLSDB = $EnvServer.databases[$PLSDBName]
+
+    if ($SkipPWB -eq $false) {
+        [string]$PWBDBName = Get-tssDatabaseName -SQLServer $EnvServer -Environment $Environment -SubEnvironment $SubEnvironment -Database PLSPWB
+        if ($PWBDBName -eq $null -or $PWBDBName.Trim() -eq '') {
+            Write-Error "No es posible conectar a la base de datos PWB";
+            return $null
+        }
+        $PWBDB = $EnvServer.databases[$PWBDBName]
+    }
+
+    $logins = New-Object -TypeName System.Collections.ArrayList
+
+    if ($Environment -cin ('DEV','UAT')){
+        $logins.Add('tssuser')
+    }
+    if ($Environment -cin ('QA','INT','UAT','PERF')){
+        $logins.Add('xpouser')
+    }
+
+    foreach ($login in $logins){
+        Write-Verbose "Verificando si el login $login existe"
+        if ($EnvServer.logins.contains($login)){
+            Write-Verbose "============================================"
+            Write-Verbose "Configurando permisos para base de datos PLS"
+            Write-Verbose "============================================"
+            Write-Verbose "Verificando si el usuario $login existe"
+            if ($PLSDB.Users.Contains($login) -eq $false){
+                $Newuser = New-Object ('Microsoft.SqlServer.Management.Smo.User') ($PLSDB, $login)
+                $Newuser.login = $login
+                $Newuser.create()
+            }
+            Write-Verbose "Asociando los roles de $login"
+            $PLSDB.Roles['db_datareader'].AddMember($login)
+            $PLSDB.Roles['db_datawriter'].AddMember($login)
+            if ($Environment -eq 'DEV'){
+                $PLSDB.Roles['db_owner'].AddMember($login)
+            }
+
+            Write-Verbose "Asociando los permisos de objeto de $login"
+            $permission = New-Object -typeName Microsoft.SqlServer.Management.Smo.ObjectPermissionSet
+            $permission.Receive = $true
+            $PLSDB.ServiceBroker.Queues['//XPO/RailOptimizer/DataServices/NotificationsTargetQueue'].grant($permission,$login)
+            $permission = New-Object -typeName Microsoft.SqlServer.Management.Smo.ObjectPermissionSet
+            $permission.Execute = $true
+            $PLSDB.Schemas['dbo'].Grant($permission,$login)
+            $PLSDB.Schemas['es'].Grant($permission,$login)
+            $PLSDB.Schemas['Tzdb'].Grant($permission,$login)
+            $PLSDB.Schemas['xpo_portal'].Grant($permission,$login)
+
+            if ($SkipPWB -eq $false) {
+                Write-Verbose "============================================"
+                Write-Verbose "Configurando permisos para base de datos PWB"
+                Write-Verbose "============================================"
+                Write-Verbose "Verificando si el usuario $login existe"
+                if ($PWBDB.Users.Contains($login) -eq $false){
+                    $Newuser = New-Object ('Microsoft.SqlServer.Management.Smo.User') ($PWBDB, $login)
+                    $Newuser.login = $login
+                    $Newuser.create()
+                }
+                Write-Verbose "Asociando los roles de $login"
+                $PWBDB.Roles['db_datareader'].AddMember($login)
+                $PWBDB.Roles['db_datawriter'].AddMember($login)
+                if ($Environment -eq 'DEV'){
+                    $PWBDB.Roles['db_owner'].AddMember($login)
+                }
+                
+                Write-Verbose "Asociando los permisos de objeto de $login"
+                $permission = New-Object -typeName Microsoft.SqlServer.Management.Smo.ObjectPermissionSet
+                $permission.Execute = $true
+                $PWBDB.Schemas['dbo'].Grant($permission,$login)
+            }
+        }
+    }
+}
+Set-tssLoginPermissions -Environment DEV -SubEnvironment PRD_OLD  -Verbose
