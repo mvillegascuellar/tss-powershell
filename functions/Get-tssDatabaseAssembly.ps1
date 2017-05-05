caff8ac24ba1929624ace7abde69d36c987e829c
mvillegascuellar <mvillegascuellar@gmail.com>
2017-05-05 16:51:49 -0500

    Funcion central para obtener objeto DB

---
+++ functions/Get-tssDatabaseAssembly.ps1
@@ -0,0 +1,29 @@
+﻿function Get-tssDatabaseAssembly {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true)]
+        [object] $Database,
+
+        [parameter(Mandatory = $true, ValueFromPipeline = $True)]
+        [string[]] $AssemblyNames
+    )
+
+    BEGIN {
+        $DatabaseAssemblies = New-Object -TypeName System.Collections.ArrayList
+    }
+    
+    PROCESS {
+        foreach ($AssemblyName in $AssemblyNames){
+            foreach ($assembly in $Database.assemblies){
+                if ($assembly.name -eq $AssemblyName){
+                    $DatabaseAssemblies.Add($assembly) | Out-Null
+                }
+            }
+        }
+    }  
+    
+    END {
+        Write-Output $DatabaseAssemblies
+    }
+
+}
